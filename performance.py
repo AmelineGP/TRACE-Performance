d3f98e7b375f524156a5b1e6828b3655d3da7d26
AmelineGP <51084484+AmelineGP@users.noreply.github.com>
2020-10-12 23:07:35 +0200

    V1.1

--- performance.py
+++ performance.py
@@ -2,8 +2,8 @@
 from xml.etree.ElementTree import ParseError
 import argparse
 import json
-import uuid
-import time'''
+import uuid'''
+import time
 import os
 '''import re
 import geojson
@@ -19,7 +19,6 @@ def openlogfile(name):
     lname=len(file_name)
     for file in complete_list:
         if file[0:lname]==file_name:
-            print(file," log file will be used for the test")
             writePerfFile(PERF_FILE,str(file)+" log file will be used for the test")
             f=open(file,'r')
             if f.mode=="r":
@@ -27,11 +26,12 @@ def openlogfile(name):
 
     return content
 
-def writePerfFile(file,line):
+def writePerfFile(title,line):
     print(line)
-    f=open(file,'w')
-    if f.mode=="w":
-        f.write(line)
+    file=title+"_"+time.strftime("%Y%m%d")+".txt"
+    f=open(file,'a')
+    if f.mode=="a":
+        f.write(line+"\n")
     return
 
 def getLineTime(text):
@@ -57,9 +57,13 @@ def getTimeLogWrittenBefore(log,str,start,end): #look for the string in the log 
 
     return str_t
 
+def allValInstring(values,str):
+    for val in values:
+        if val not in str: return False
+    return True
 
 
-def getDelay(log,action_str,result_str):
+def getDelay(log,action_str,result_str,action_str2='',result_str2=''):
     action=True #set to 'true' when we are looking for the log corresponding to the input, and 'fqlse' when we are looking for the one for the output
 
     action_t=0 #time of the action
@@ -68,40 +72,38 @@ def getDelay(log,action_str,result_str):
     delays=[]
     for line in log:
         if action :
-            if len(action_str)==1 and action_str[0] in line["text"]:#we are looking for one simple string
-                print("action:",line)
-                action_t=line["timesec"]
-                action=False
-                continue
-            if len(action_str)>1 and action_str[0] in line["text"]: #we are looking for two strings
-                action_t=getTimeLogWrittenBefore(log,action_str[1], line["id"]-20,line["id"])#looking for the second string written
-                if action_t==0:continue #the second string was not found
-                action=False
-                continue
+            if allValInstring(action_str,line["text"]):
+                if action_str2=='':  #we are looking for a string containing all the strings in action_str
+                    action_t=line["timesec"]
+                    action=False
+                    continue
+                if action_str2!='': #we are looking for two strings
+                    action_t=getTimeLogWrittenBefore(log,action_str2, line["id"]-20,line["id"])#looking for the second string written
+                    if action_t==0:continue #the second string was not found
+                    action=False
+                    continue
         if action==False:
-            if len(result_str)==1 and result_str[0] in line["text"]:#we are looking for one simple string
-                print("result:",line)
-                action=True
-                result_t=line["timesec"]
-                delays.append(result_t-action_t)
-                action_t=result_t=0
-                continue
-            if len(result_str)>1 and result_str[0] in line["text"]: #we are looking for two strings
-                result_t=getTimeLogWrittenBefore(log,result_str[1], line["id"]-20,line["id"])#looking for the second string written
-                action=True
-                if action_t==0:continue #the second string was not found
-                result_t=line["timesec"]
-                delays.append(result_t-action_t)
-                action_t=result_t=0
-
-
+            if allValInstring(result_str,line["text"]):
+                if result_str2=='':#we are looking for one simple string
+                    action=True
+                    result_t=line["timesec"]
+                    delays.append(result_t-action_t)
+                    action_t=result_t=0
+                    continue
+                if result_str2!='': #we are looking for two strings
+                    result_t=getTimeLogWrittenBefore(log,result_str2, line["id"]-20,line["id"])#looking for the second string written
+                    action=True
+                    if action_t==0:continue #the second string was not found
+                    result_t=line["timesec"]
+                    delays.append(result_t-action_t)
+                    action_t=result_t=0
 
     return delays
 
 def delayRoleOpening(logs):
     avDelay=0 #average delay of role opening
     for role in ROLE_NAMES:
-        delays=getDelay(logs,[ROLE_OPEN_ACTION+role,ROLE_OPEN_ACION_2],[ROLE_OPEN_RESULT+role])
+        delays=getDelay(logs,[ROLE_OPEN_ACTION+role],[ROLE_OPEN_RESULT+role],ROLE_OPEN_ACION_2)
         if len(delays)>0: #if there is at least one time calculated
             writePerfFile(PERF_FILE,"Delays to open the role "+role+": "+str(delays))
             for delay in delays:
@@ -121,6 +123,44 @@ def delayRoleOpening(logs):
     writePerfFile(PERF_FILE,"the average delay to activate an already open role is:"+ str(avDelay))
 
     return'''
+def getDelayStringWithCallsign(logs, action_str,result_str,action_sep='',):
+    delays=[]
+    for line in logs: #looking for the callsign contained in the ligne "action_str"+callsign
+        if action_str in line['text']:
+            callsign=''
+            for letter in line['text'][len(action_str):]:
+                if letter!=action_sep:callsign+=letter
+                else: break
+            delays+=getDelay(logs,[action_str,callsign],[result_str,callsign])
+
+    return delays
+
+def delayCLDForward(logs):
+    avDelay=0
+    delays=getDelayStringWithCallsign(logs,CLD_FORWARD_ACTION,CLD_FORWARD_RESULT,')')
+    writePerfFile(PERF_FILE,"Delays to move a FS from the wait list to the ground zone: "+str(delays))
+    for delay in delays:
+        avDelay=(avDelay+delay)/2
+    writePerfFile(PERF_FILE,"the average delay to move a FS from the wait liste to the ground zone:"+ str(avDelay))
+    return
+
+def delayTakeOff(logs):
+    avDelay=0
+    delays=getDelayStringWithCallsign(logs,TAKE_OFF_ACTION,TAKE_OFF_RESULT,')')
+    writePerfFile(PERF_FILE,"Delays to move a FS from the ground zone to the departed zone: "+str(delays))
+    for delay in delays:
+        avDelay=(avDelay+delay)/2
+    writePerfFile(PERF_FILE,"the average delay to move a FS from the ground zone to the departed zone:"+ str(avDelay))
+    return
+
+def delayWaitListe(logs):
+    avDelay=0
+    delays=getDelayStringWithCallsign(logs,WAIT_LISTE_ACTION,WAIT_LISTE_RESULT,'/')
+    writePerfFile(PERF_FILE,"Delays to move a FS from the wait liste to the ground zone: "+str(delays))
+    for delay in delays:
+        avDelay=(avDelay+delay)/2
+    writePerfFile(PERF_FILE,"the average delay to move a FS from the wait liste to the ground zone:"+ str(avDelay))
+    return
 
 ##### Global Variable#######
 ROLE_NAMES={'CLD_1','SPVR','GRO_1','GRO_2','ADC_1','ADC_2','ADC_3','DEP_1','DEP_2','FIN_1','FIN_2','FIN_3','APW_1','APW_2'}
@@ -129,10 +169,22 @@ ROLE_OPEN_RESULT= "set role on front: "
 ROLE_OPEN_ACION_2="Button OK_BUTTON pressed."
 ROLE_CHANGE_ACTION=" change status ACTIVE_AND_SHOWN"
 
-PERF_FILE='performance.txt'
+CLD_FORWARD_ACTION="    LMB pressed on CLD_FORWARD (strip "
+CLD_FORWARD_RESULT="CLD_1 - Candidate visible strips in site.eStripboard.ifr.twr.CldGroundZone:"
+
+TAKE_OFF_ACTION="    LMB pressed on TAKE_OFF_BUTTON (strip "
+TAKE_OFF_RESULT="DepartedZone:"
+
+WAIT_LISTE_ACTION="    Double click on "
+WAIT_LISTE_RESULT="CLD_1 - Candidate visible strips in site.eStripboard.ifr.twr.CldWaitListZone:"
+
+PERF_FILE='performance'
 ##### Global Variable#######
 
 logs=openlogfile('client')
 dictLogs=getLineTime(logs)
-delayRoleOpening(dictLogs)
+#delayRoleOpening(dictLogs)
+delayWaitListe(dictLogs)
+delayCLDForward(dictLogs)
+delayTakeOff(dictLogs)
 #delayRoleChange(dictLogs)
